901fb6d07b18aacdd54b5e7b248a2cbf99e92f55
motoki42 <a140343y@st.u-gakugei.ac.jp>
2024-05-13 17:59:21 +0900

    Make EmployeeService

---
+++ src/main/java/com/example/service/EmployeeService.java
@@ -0,0 +1,28 @@
+package com.example.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+/**
+ * @author motokidoi
+ */
+
+import com.example.domain.Employee;
+import com.example.repository.EmployeeRepository;
+@Service
+@Transactional
+public class EmployeeService {
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    /**
+     * 従業員情報を全件取得する。
+     * @return
+     */
+    public List<Employee> showList(){
+        List<Employee> employeeList = employeeRepository.findAll();
+        return employeeList;
+    }
+}
