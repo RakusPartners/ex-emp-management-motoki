5740633bd2ff9fbcb9ef64e131d3ec58051d684a
motoki42 <a140343y@st.u-gakugei.ac.jp>
2024-05-13 16:49:41 +0900

    insert method commit

--- src/main/java/com/example/service/AdministratorService.java
+++ src/main/java/com/example/service/AdministratorService.java
@@ -4,7 +4,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.example.domain.Administrator;
 import com.example.repository.AdministratorRepository;
+
 /**
  * @author motokidoi
  */
@@ -13,4 +15,13 @@ import com.example.repository.AdministratorRepository;
 public class AdministratorService {
     @Autowired
     private AdministratorRepository administratorRepository;
+
+    /**
+     * 管理者情報を挿入する
+     * 
+     * @param administrator
+     */
+    public void insert(Administrator administrator) {
+        administratorRepository.insert(administrator);
+    }
 }
