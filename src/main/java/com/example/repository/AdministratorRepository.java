a6ef136a8f5ed52ffe578992de494a15876e8e7e
motoki42 <a140343y@st.u-gakugei.ac.jp>
2024-05-14 11:27:30 +0900

    fixed detail.html insert.html

--- src/main/java/com/example/repository/AdministratorRepository.java
+++ src/main/java/com/example/repository/AdministratorRepository.java
@@ -9,7 +9,6 @@ import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.jdbc.core.namedparam.SqlParameterSource;
 import org.springframework.stereotype.Repository;
-import org.springframework.web.bind.annotation.GetMapping;
 
 import com.example.domain.Administrator;
 /**
